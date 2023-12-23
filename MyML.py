9128e55e9d6e6d3547c0bc6d8063096d5ae3cd90
Ivan Putra Pratama <ifam4@yahoo.com>
2023-12-23 15:41:01 +0700

    Menambahkan MSE,MAE,Raquared

--- MyML.py
+++ MyML.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 st.write("# Predict Sales with Linear Regression")
 
@@ -125,3 +128,24 @@ ax.legend()  # Menampilkan legenda
 
 # Menampilkan plot di Streamlit
 st.pyplot(fig)
+
+X = sorteddf1[['X1']]
+y = sorteddf1['Y1']  
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+
+mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+r_squared = r2_score(y_test, y_pred)
+
+
+st.write(f"Mean Squared Error (MSE): {mse}")
+st.write(f"Mean Absolute Error (MAE): {mae}")
+st.write(f"R-squared (R²): {r_squared}")
+
+
